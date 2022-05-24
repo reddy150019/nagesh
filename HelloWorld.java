be5e7a0cb45d5775fb679e7ada105e6007f2f2da
EC2 Default User <ec2-user@ip-172-31-37-152.ap-south-1.compute.internal>
2022-05-24 11:31:33 +0000

    added hello jaca

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, nagesh");
+    System.out.println("Hello, hari");
+    System.out.println("Hello, hari");
+    System.out.println("Hello, hari");
+    System.out.println("Hello, siva");
+    System.out.println("Hello, siva");
+  }
+}
